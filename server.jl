d824e4364af3366aceb19c2296d1fc3f454fec78
ellblan <viora.neo@gmail.com>
2025-12-21 21:43:16 +0900

    Phase7

--- server.jl
+++ server.jl
@@ -1,15 +1,12 @@
 using HTTP
 
 HTTP.listen("0.0.0.0", 10000) do stream::HTTP.Stream
-    while !eof(stream)
-        req = HTTP.request(stream)
-        path = HTTP.URI(req.target).path
-        
-        if path == "/health"
-            HTTP.write(stream, HTTP.Response(200, "GrowthTrail AI Live! ✓"))
-        else
-            HTTP.write(stream, HTTP.Response(404, "Not Found"))
-        end
+    reqs = HTTP.read(stream)
+    req = reqs[1]
+    
+    if HTTP.URI(req.target).path == "/health"
+        HTTP.write(stream, HTTP.Response(200, "GrowthTrail AI Live! ✓"))
+    else
+        HTTP.write(stream, HTTP.Response(404, "Not Found"))
     end
-    HTTP.close(stream)
 end
